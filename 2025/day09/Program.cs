4d208fe3e89d2cab06f380ea28c5c83993018b9e
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2025-12-09 11:08:30 +0100

    Solve 2025 day 09, part 1
    Well, that was easy!

---
+++ 2025/day09/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Linq;
+
+class Point {
+    public int X { get; }
+    public int Y { get; }
+
+    public Point(string line) {
+        var parts = line.Split(',');
+        X = Int32.Parse(parts[0]);
+        Y = Int32.Parse(parts[1]);
+    }
+
+    public long AreaTo(Point that) {
+        long w = Int32.Abs(this.X - that.X) + 1;
+        long h = Int32.Abs(this.Y - that.Y) + 1;
+        return w * h;
+    }
+}
+
+class Solver
+{
+    static void Main(string[] args)
+    {
+        string filename = "input";
+        if (args.Length > 0) {
+            filename = args[0];
+        }
+        List<Point> points = new();
+        using (StreamReader sr = new StreamReader(filename))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null) {
+                points.Add(new Point(line));
+            }
+        }
+        var maxArea = points
+            .Select((p, i) =>
+                // Must make sure that the sequence contains *something*, so waste linear time here
+                // to include the area 1 "rectangle" with itself.
+                points[i..].Select(q =>
+                    p.AreaTo(q)
+                )
+                .Max()
+            )
+            .Max();
+        Console.WriteLine($"Max area: {maxArea}");
+    }
+}
